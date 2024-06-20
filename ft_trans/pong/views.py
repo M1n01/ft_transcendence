cbfc2f510846446b8968f556f129997ab933bff4
hsano <hsano@student.42tokyo.jp>
2024-06-20 23:13:26 +0900

    Etag導入して変更ないときはキャッシュを利用する

--- ft_trans/pong/views.py
+++ ft_trans/pong/views.py
@@ -7,7 +7,11 @@ from django.views.decorators.csrf import csrf_protect
 from django.http import Http404
 from urllib.parse import urlparse
 from django.template import RequestContext, Template
+from django.views.decorators.http import condition
+import django.views.decorators.http
 import asyncio
+import hashlib
+from django.conf import settings
 
 def checkSPA(request):
     spa = request.META.get('HTTP_SPA')
@@ -26,28 +30,32 @@ class Pong(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+def my_etag(request, *args, **kwargs):
+    return (hashlib.md5(':'.join( request.GET.dict().values()).encode('utf-8')).hexdigest())
 
 
 # テスト用　後で消す
+@condition(etag_func=my_etag)
 def lang(request):
-    #template = Template("{{ title }}: {{ ip_address }}")
-    #context = RequestContext(request, [ip_address_processor])
-    #return HttpResponse(template.render(context))
     return render(request, 'pong/lang_test.html')
 
+@condition(etag_func=my_etag)
 def script(request):
     checkSPA(request)
     return render(request, 'pong/script.html')
 
 
+@condition(etag_func=my_etag)
 def script2(request):
     return render(request, 'pong/script2.html')
 
 
+@condition(etag_func=my_etag)
 def test(request):
     return render(request, 'pong/test.html')
 
 
+@condition(etag_func=my_etag)
 def index(request):
     context = {
         "latest_question_list": "abcdefg",
