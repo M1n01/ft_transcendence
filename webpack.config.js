a092dec4bc9ef80418cea82dd225a9976da5a1db
M1n01 <abe.minato.bz@gmail.com>
2024-07-04 18:28:01 +0900

    feat: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = {
-  ...
-  resolve: {
-    alias: {
-      '@bar': path.resolve(__dirname, 'src/foo/bar')
-    }
-  }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './ft_trans/spa/templates/spa/index.html',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
