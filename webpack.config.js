17ac153631d3d7bdf5ce640359f0fa43733cea77
M1n01 <abe.minato.bz@gmail.com>
2024-07-09 17:49:04 +0900

    feat: webpack実装

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,26 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: './ft_trans/spa/templates/spa/index.html',
+  entry: './ft_trans/spa/static/spa/js/index.js',
   output: {
+    path: __dirname + '/ft_trans/spa/static/dist',
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      '@': __dirname,
+    },
   },
 };
