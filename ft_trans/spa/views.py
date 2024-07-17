aa9a3b5385a3ebb95f141569504f5d8bc584a5ed
hsano <hsano@student.42tokyo.jp>
2024-07-17 21:22:12 +0900

    Webpack Test

--- ft_trans/spa/views.py
+++ ft_trans/spa/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import asyncio
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 def index(request):
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,42 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+//import webpack from 'webpack';
+import BundleTracker from 'webpack-bundle-tracker';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: './ft_trans/spa/static/spa/js/index.js',
+  mode: 'development',
+  //context: path.resolve(__dirname, 'ft_trans/'),
+  entry: './ft_trans/assets/js/index',
+  //entry: './ft_trans/spa/static/spa/js/index.js',
+  //entry: './ft_trans/spa/static/spa/js/index.js',
   output: {
-    path: __dirname + '/ft_trans/spa/static/dist',
-    filename: 'bundle.js',
+    path: __dirname + '/ft_trans/assets/webpack_bundles',
+    //filename: 'bundle.js',
+    //path: path.resolve(__dirname, 'ft_trans', 'assets', 'webpack_bundles'),
+    publicPath: 'http://localhost:3000/frontend/webpack_bundles/',
+    //publicPath: 'auto',
+    filename: '[name]-[hash].js',
   },
+  devtool: 'source-map',
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+    host: 'localhost',
+    port: 3000,
+    // Allow CORS requests from the Django dev server domain:
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
+  plugins: [
+    new BundleTracker({
+      path: path.resolve(__dirname, 'ft_trans', 'assets'),
+      filename: 'webpack-stats.json',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
   module: {
     rules: [
       {
@@ -20,9 +47,14 @@ export default {
           presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
   resolve: {
+    extensions: ['.js'],
     alias: {
       '@': path.resolve(__dirname, 'ft_trans/spa/static/spa/js'),
       '~': __dirname,
