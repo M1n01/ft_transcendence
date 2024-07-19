83174d2fe773859e27fb090dd85a6889ddd00108
Minato Abe <m.abe.howcollect@gmail.com>
2024-07-19 16:36:10 +0900

    #9 style: solhint .sol

--- blockchain/contracts/ScoreKeeper.sol
+++ blockchain/contracts/ScoreKeeper.sol
@@ -1,19 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 
 contract ScoreKeeper is Ownable {
-    constructor(address initialOwner) Ownable(initialOwner) {
-    }
+  constructor(address initialOwner) Ownable(initialOwner) {}
 
-    mapping(address => uint256) private scores;
+  mapping(address => uint256) private scores;
 
-    function setScore(uint256 _score) public {
-        scores[msg.sender] = _score;
-    }
+  function setScore(address _user, uint256 _score) public onlyOwner {
+    scores[_user] = _score;
+  }
 
-    function getScore(address _user) public view returns (uint256) {
-        return scores[_user];
-    }
+  function getScore(address _user) public view returns (uint256) {
+    return scores[_user];
+  }
 }
