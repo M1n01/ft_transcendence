3f0f051c4a250158c07f661545d7654a62a9ed3c
Minato Abe <m.abe.howcollect@gmail.com>
2024-07-19 16:25:47 +0900

    #9 update: contract test

--- blockchain/test/ScoreKeeper.js
+++ blockchain/test/ScoreKeeper.js
@@ -1,14 +1,22 @@
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
 import { describe, it } from 'mocha';
+import { ethers } from 'hardhat';
 
 describe('ScoreKeeper contarct', function () {
   it('Should set and get the score of an address', async function () {
+    const [owner, otherAccount] = await ethers.getSigners();
     const ScoreKeeper = await ethers.getContractFactory('ScoreKeeper');
-    const scoreKeeper = await ScoreKeeper.deploy();
+    const scoreKeeper = await ScoreKeeper.deploy(owner.address);
     await scoreKeeper.waitForDeployment();
 
-    const [owner] = await ethers.getSigners();
-    expect(await scoreKeeper.getScore(owner.address)).to.equal(0);
+    await scoreKeeper.setScore(owner.address, 100);
+    expect(await scoreKeeper.getScore(owner.address)).to.equal(100);
+
+    await scoreKeeper.setScore(otherAccount.address, 200);
+    expect(await scoreKeeper.getScore(otherAccount.address)).to.equal(200);
+
+    await expect(scoreKeeper.connect(otherAccount).setScore(owner.address, 300)).to.be.revertedWith(
+      'Ownable: caller is not the owner'
+    );
   });
 });
