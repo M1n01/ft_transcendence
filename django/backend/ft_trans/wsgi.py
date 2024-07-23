ad639b599cb066adf2184bb8231658ac04ac19c0
M1n01 <abe.minato.bz@gmail.com>
2024-07-24 06:18:04 +0900

    style: python format

--- django/backend/ft_trans/wsgi.py
+++ django/backend/ft_trans/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ft_trans.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ft_trans.settings")
 
 application = get_wsgi_application()
