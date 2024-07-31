9b72dbb7278950953d958a1cbc8e8526cd2cc3f9
M1n01 <abe.minato.bz@gmail.com>
2024-07-31 15:34:58 +0900

    #9 delete: scorekeeper page

--- django/frontend/src/spa/js/routing/routes.js
+++ django/frontend/src/spa/js/routing/routes.js
@@ -12,7 +12,6 @@ import Index from '../views/Index.js';
 import Pong from '../views/Pong.js';
 import Admin from '../views/Admin.js';
 import LoginSuccess from '../views/LoginSuccess.js';
-import ScoreKeeper from '../views/ScoreKeeper.js';
 
 export const Routes = [
   { path: '/', view: Dashboard },
@@ -31,5 +30,4 @@ export const Routes = [
   { path: '/scriptt', view: Script2 },
   { path: '/script', view: Script },
   { path: '/success-login', view: LoginSuccess },
-  { path: '/score-keeper', view: ScoreKeeper },
 ];
