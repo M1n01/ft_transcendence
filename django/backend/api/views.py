e0e2b6465ea6530a86936825c47af77d44fce1da
M1n01 <abe.minato.bz@gmail.com>
2024-09-01 15:27:54 +0900

    #9 Update: SaveGameScoreView to use Web3 instead of web3

--- django/backend/api/views.py
+++ django/backend/api/views.py
@@ -16,57 +16,71 @@ from solcx import compile_source, install_solc, set_solc_version
 
 class SaveGameScoreView(APIView):
     def __init__(self):
-        self.web3 = Web3(Web3.HTTPProvider("http://contracts:8545"))
-        # self.web3.eth.set_gas_price_strategy(medium_gas_price_strategy)  # ガス価格を設定
-        self.account = self.web3.eth.accounts[0]  # ノードのaccount[0]を使用
+        self.w3 = Web3(Web3.HTTPProvider("http://eth:8545"))
+        # self.w3.eth.set_gas_price_strategy(medium_gas_price_strategy)  # ガス価格を設定
+        self.account = self.w3.eth.account.from_key(settings.PRIVATE_KEY)
         self.setup_contract()
 
     def setup_contract(self):
         try:
             contract = SmartContract.objects.get(name="ScoreKeeper")
-            self.game_contract = self.web3.eth.contract(
+            self.game_contract = self.w3.eth.contract(
                 address=contract.address, abi=contract.abi
             )
         except SmartContract.DoesNotExist:
-            compiled_sol = self.compile_source_file()
-            contract_id, contract_interface = compiled_sol.popitem()
+            contract_interface = self.get_hardhat_compiled_contract()
             address = self.deploy_contract(contract_interface)
             SmartContract.objects.create(
                 name="ScoreKeeper", address=address, abi=contract_interface["abi"]
             )
-            self.game_contract = self.web3.eth.contract(
+            self.game_contract = self.w3.eth.contract(
                 address=address, abi=contract_interface["abi"]
             )
 
-    def compile_source_file(self):
-        install_solc(version="0.8.24")
+    # def compile_source_file(self):
+    #     install_solc(version="0.8.24")
+    #     contract_path = os.path.join(
+    #         settings.BLOCKCHAIN_DIR, "contracts", "ScoreKeeper.sol"
+    #     )
+    #     with open(contract_path, "r") as file:
+    #         contract_source_file = file.read()
+
+    #     compiled_sol = compile_source(
+    #         contract_source_file, output_values=["abi", "bin"]
+    #     )
+    #     return compiled_sol
+
+    def get_hardhat_compiled_contract(self):
         contract_path = os.path.join(
-            settings.BLOCKCHAIN_DIR, "contracts", "ScoreKeeper.sol"
+            settings.BLOCKCHAIN_DIR,
+            "artifacts",
+            "contracts",
+            "ScoreKeeper.sol",
+            "ScoreKeeper.json",
         )
         with open(contract_path, "r") as file:
-            contract_source_file = file.read()
+            contract_json = json.load(file)
 
-        compiled_sol = compile_source(
-            contract_source_file, output_values=["abi", "bin"]
-        )
-        return compiled_sol
+        return {"abi": contract_json["abi"], "bin": contract_json["bytecode"]}
 
     def deploy_contract(self, contract_interface):
-        contract = self.web3.eth.contract(
+        contract = self.w3.eth.contract(
             abi=contract_interface["abi"], bytecode=contract_interface["bin"]
         )
 
-        contract_owner = [self.account.address]
+        address = self.account.address
 
-        transaction = contract.constructor(contract_owner).build_transaction(
+        transaction = contract.constructor(address).build_transaction(
             {
-                "from": self.account,
-                "nonce": self.web3.eth.get_transaction_count(self.account),
+                "from": address,
+                "nonce": self.w3.eth.get_transaction_count(address),
             }
         )
-        signed_txn = self.web3.eth.account.sign_transaction(transaction)
-        tx_hash = self.web3.eth.send_raw_transaction(signed_txn.raw_transaction)
-        tx_receipt = self.web3.eth.wait_for_transaction_receipt(tx_hash)
+        signed_txn = self.w3.eth.account.sign_transaction(
+            transaction, settings.PRIVATE_KEY
+        )
+        tx_hash = self.w3.eth.send_raw_transaction(signed_txn.raw_transaction)
+        tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
         return tx_receipt.contractAddress
 
     def get_contract(self):
@@ -121,18 +135,16 @@ class SaveGameScoreView(APIView):
                 serializer.validated_data["loser_score"],
             ).build_transaction(
                 {
-                    "from": os.getenv("CONTRACT_OWNER_ADDRESS"),
-                    "nonce": self.web3.eth.get_transaction_count(
-                        os.getenv("CONTRACT_OWNER_ADDRESS")
-                    ),
+                    "from": self.account.address,
+                    "nonce": self.w3.eth.get_transaction_count(self.account.address),
                 }
             )
 
-            signed_txn = self.web3.eth.account.sign_transaction(
-                transaction, private_key=os.getenv("CONTRACT_OWNER_PRIVATE_KEY")
+            signed_txn = self.w3.eth.account.sign_transaction(
+                transaction, settings.PRIVATE_KEY
             )
-            tx_hash = self.web3.eth.send_raw_transaction(signed_txn.raw_transaction)
-            receipt = self.web3.eth.wait_for_transaction_receipt(tx_hash)
+            tx_hash = self.w3.eth.send_raw_transaction(signed_txn.raw_transaction)
+            receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
 
             return Response(
                 {
