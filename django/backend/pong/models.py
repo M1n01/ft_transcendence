76efdcddb7db350eb2ac807d1db9a2e2567b8501
M1n01 <abe.minato.bz@gmail.com>
2024-09-08 10:25:40 +0900

    #9 #57 add: Tournament model

--- django/backend/pong/models.py
+++ django/backend/pong/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Tournament(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    max_players = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    current_players = models.IntegerField()
+
+
+class Match(models.Model):
+    tournament_id = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    winner = models.IntegerField()
+    loser = models.IntegerField(allow_null=True) # シード(不戦勝)の場合はnull
+    winner_score = models.SmallIntegerField()
+    loser_score = models.SmallIntegerField(allow_null=True)
+    round = models.SmallIntegerField()
+
+    def __str__(self):
+        return f"id={self.id}: {self.winner} vs {self.loser} (winner_score={self.winner_score}, loser_score={self.loser_score})"
