5b736acbd8ce81f24cbefbfd8674005f8ce27ce2
M1n01 <abe.minato.bz@gmail.com>
2024-09-10 20:31:10 +0900

    #9 feat: Add validation and Create initial data in Tournament model

--- django/backend/pong/models.py
+++ django/backend/pong/models.py
@@ -4,12 +4,15 @@ from django.db import models
 class Tournament(models.Model):
     id = models.BigAutoField(primary_key=True)
     start_at = models.DateTimeField()
-    current_players = models.IntegerField(min_value=4, max_value=32)
+    current_players = models.IntegerField()
 
     def save(self, *args, **kwargs):
         if self.current_players < 4 or self.current_players > 32:
             raise ValueError("current_players must be between 4 and 32")
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"id={self.id}: {self.start_at} ({self.current_players} players)"
 
 
 """
@@ -24,9 +27,9 @@ class Match(models.Model):
     round = models.SmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     player1 = models.IntegerField()
-    player2 = models.IntegerField(null=True, blank=True)  # シード(不戦勝)の場合はnull
-    player1_score = models.SmallIntegerField(blank=True)
-    player2_score = models.SmallIntegerField(null=True, blank=True)
+    player2 = models.IntegerField(null=True)  # シード(不戦勝)の場合はnull
+    player1_score = models.SmallIntegerField(null=True)
+    player2_score = models.SmallIntegerField(null=True)
 
     def __str__(self):
         return f"id={self.id}: {self.player1} vs {self.player2} (player1_score={self.player1_score}, player2_score={self.player2_score})"
