029eafd080a6d36fc3290db94614bde895e5a915
hsano <hsano@student.42tokyo.jp>
2024-09-23 23:04:46 +0900

    fix redis　tls対応中

--- django/backend/ft_trans/settings_dev.py
+++ django/backend/ft_trans/settings_dev.py
@@ -215,15 +215,7 @@ AUTHENTICATION_BACKENDS = [
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://172.38.10.30:6379",
-        # "OPTIONS": {
-        #    "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        #    "CONNECTION_POOL_KWARGS": {"ssl_cert_reqs": None},
-        # },
-        # "OPTIONS": {
-        # "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        # "PASSWORD": "foobared",
-        # },
+        "LOCATION": f"rediss://default:{os.environ['REDIS_PASSOWRD']}@172.38.10.30:6379",
     }
 }
 
@@ -241,7 +233,7 @@ CELERY_CACHE_BACKEND = "django-cache"
 
 # Celery設定
 # CELERY_BROKER_URL = os.environ.get("REDIS_URL", "redis://redis:6379/1")
-CELERY_BROKER_URL = "redis://172.38.10.30:6379"
+CELERY_BROKER_URL = f"rediss://default:{os.environ['REDIS_PASSOWRD']}@172.38.10.30:6379"
 CELERY_RESULT_BACKEND = "django-db"
 
 CELERY_RESULT_EXTENDED = True
--- docker/redis/make_ca.sh
+++ docker/redis/make_ca.sh
@@ -1,117 +1,54 @@
-openssl req -x509 -days 3650 -nodes -newkey rsa:4096 -keyout /etc/tls/redis.key -out /etc/tls/redis.crt -config - << __EOF__
-[req]
-distinguished_name = req_distinguished_name
-prompt = no
+openssl genrsa 4096 > ca.key
+openssl rsa -text -noout -in ca.key
+openssl rsa -in ca.key -pubout -out ca-public.key
 
-[req_distinguished_name]
-C = JP
-ST = Aichi
-L = Toyota-shi
-O = 42
-OU = 42tokyo
-CN = hsano.42.fr
-
-[v3_req]
-keyUsage = TestInception
-extendedKeyUsage = serverAuth
-subjectAltName = @alt_names
-
-[alt_names]
-DNS.1 = mydomain.example
-DNS.2 = sub.mydomain.example
-__EOF__
-
-# 証明書の内容を確認
-#openssl x509 -in /etc/tls/redis.crt -noout -text
-
-# CA証明書と秘密鍵の生成
-#openssl genrsa -out /tmp/ca-key.pem 4096
-#openssl req -x509 -new -nodes -key /tmp/ca-key.pem -sha256 -days 3650 -out /tmp/ca-cert.pem
+ssh-keygen -f id_rsa.pub -e -m pem
+openssl req -new -key ca.key -subj "/CN=rootca" > ca.csr
+openssl x509 -text -noout -in ca.crt
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=rootca" -sha256 -days 10000 -out ca.crt
 
 
-openssl genrsa -out /etc/tls/ca-key.pem 4096
-openssl req -x509 -new -nodes -key /etc/tls/ca-key.pem -sha256 -days 3650 -out /etc/tls/ca-cert.pem   -config - << __EOF__
-  [req]
-  distinguished_name = req_distinguished_name
-  prompt = no
+openssl genrsa 4096 > server.key
+openssl rsa -text -noout -in server.key
+openssl rsa -in server.key -pubout -out server-public.key
+openssl req -new -key server.key -subj "/CN=servername" > server.csr
+openssl req -text -noout -in server.csr
 
-  [req_distinguished_name]
-  C = JP
-  ST = Aichi
-  L = Toyota-shi
-  O = 42
-  OU = 42tokyo
-  CN = hsano.42.fr
-
-  [v3_req]
-  keyUsage = TestInception
-  extendedKeyUsage = serverAuth
-  subjectAltName = @alt_names
-
-  [alt_names]
-  DNS.1 = mydomain.example
-  DNS.2 = sub.mydomain.example
-__EOF__
-
-
-
-# サーバー用の証明書と秘密鍵の生成
-openssl genrsa -out /etc/tls/server-key.pem 4096
-openssl req -new -key /etc/tls/server-key.pem -out /etc/tls/server.csr   -config - << __EOF__
-  [req]
-  distinguished_name = req_distinguished_name
-  prompt = no
-
-  [req_distinguished_name]
-  C = JP
-  ST = Aichi
-  L = Toyota-shi
-  O = 42
-  OU = 42tokyo
-  CN = hsano.42.fr
-
-  [v3_req]
-  keyUsage = TestInception
-  extendedKeyUsage = serverAuth
-  subjectAltName = @alt_names
-
-  [alt_names]
-  DNS.1 = mydomain.example
-  DNS.2 = sub.mydomain.example
-__EOF__
-
-openssl x509 -req -in /etc/tls/server.csr -CA /etc/tls/ca-cert.pem -CAkey /etc/tls/ca-key.pem -CAcreateserial -out /etc/tls/server-cert.pem -days 3650 -sha256
-
-
-# サーバー用の証明書と秘密鍵の生成
-#openssl genrsa -out /etc/tls/server-key.pem 4096
-#openssl req -new -key /etc/tls/server-key.pem -out /etc/tls/server.csr  -config - << __EOF__
-#[req]
-#distinguished_name = req_distinguished_name
+#openssl req -new -key server.key  -out server.csr -config - << __EOF__
+#[ req ]
+#default_bits = 2048
 #prompt = no
+#default_md = sha256
+#req_extensions = req_ext
+#distinguished_name = dn
 #
-#[req_distinguished_name]
-#C = JP
-#ST = Aichi
-#L = Toyota-shi
-#O = 42
-#OU = 42tokyo
-#CN = hsano.42.fr
+#[ dn ]
+#CN = servername
 #
-#[v3_req]
-#keyUsage = TestInception
-#extendedKeyUsage = serverAuth
+#[ req_ext ]
 #subjectAltName = @alt_names
 #
-#[alt_names]
-#DNS.1 = mydomain.example
-#DNS.2 = sub.mydomain.example
+#[ alt_names ]
+#DNS.1 = othername1
+#DNS.2 = othername2
+#
+#[ v3_ext ]
+#authorityKeyIdentifier=keyid,issuer:always
+#basicConstraints=CA:FALSE
+#keyUsage=keyEncipherment,dataEncipherment
+#extendedKeyUsage=serverAuth,clientAuth
+#subjectAltName=@alt_names
 #__EOF__
-#
-#
-#
-#openssl x509 -req -in /etc/tls/server.csr -CA /etc/tls/redis.crt -CAkey /etc/tls/redis.key -CAcreateserial -out /etc/tls/server-cert.pem -days 3650 -sha256
-#
-#
-#
-#
+
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 10000 -out server.crt
+#openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 10000 \
+  #-extensions v3_ext -extfile csr.conf \
+  #-out server.crt
+  #
+openssl x509 -text -noout -in server.crt
+
+
+cp server.key /etc/tls/server.key
+cp server.crt /etc/tls/server.crt
+cp ca.crt /etc/tls/ca/ca.crt
+cp ca.key /etc/tls/ca/ca.key
