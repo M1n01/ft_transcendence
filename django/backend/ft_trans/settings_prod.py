29b75741c97e3fe1a3229b7c091a43268d06d784
hsano <hsano@student.42tokyo.jp>
2024-10-03 02:12:27 +0900

    refactor 不要なコメントの削除

--- django/backend/ft_trans/settings_prod.py
+++ django/backend/ft_trans/settings_prod.py
@@ -315,15 +315,6 @@ LANGUAGES = [
     ("fr", _("French")),
 ]
 
-# CORS設定（暫定)
-# CORS_ALLOWED_ORIGINS = [
-# "https://localhost",
-# "http://localhost:8000",
-# ]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # HTTPS(TLS)
