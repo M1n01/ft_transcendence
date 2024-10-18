626629d6ed4683089dd86de3c32b852415c24cc0
hnoguchi <>
2024-10-18 16:05:09 +0900

    fix: [gdpr]: フォームバリデーションの修正

--- django/backend/users/forms.py
+++ django/backend/users/forms.py
@@ -8,11 +8,23 @@ from django.utils.translation import gettext_lazy as _
 
 from django.contrib.auth.forms import PasswordChangeForm
 from django.core.exceptions import ValidationError
+import logging
 import re
+
+from django.conf import settings
+
+USERNAME_MAX_LEN = getattr(settings, "USERNAME_MAX_LEN", None)
+LASTNAME_MAX_LEN = getattr(settings, "LASTNAME_MAX_LEN", None)
+FIRSTNAME_MAX_LEN = getattr(settings, "FIRSTNAME_MAX_LEN", None)
+EMAIL_MAX_LEN = getattr(settings, "EMAIL_MAX_LEN", None)
+PHONE_MAX_LEN = getattr(settings, "PHONE_MAX_LEN", None)
+PASSWORD_MIN_LEN = getattr(settings, "PASSWORD_MIN_LEN", None)
+PASSWORD_MAX_LEN = getattr(settings, "PASSWORD_MAX_LEN", None)
 
 
 class UserEditForm(forms.ModelForm):
     username = forms.CharField(
+        max_length=USERNAME_MAX_LEN,
         widget=forms.TextInput(
             attrs={
                 "id": "username_id",
@@ -22,6 +34,7 @@ class UserEditForm(forms.ModelForm):
         ),
     )
     email = forms.CharField(
+        max_length=EMAIL_MAX_LEN,
         widget=forms.TextInput(
             attrs={
                 "id": "email_id",
@@ -33,7 +46,7 @@ class UserEditForm(forms.ModelForm):
     )
     first_name = forms.CharField(
         # required=False,
-        max_length=64,
+        max_length=FIRSTNAME_MAX_LEN,
         widget=forms.TextInput(
             attrs={
                 "id": "first_name_id",
@@ -43,8 +56,8 @@ class UserEditForm(forms.ModelForm):
         ),
     )
     last_name = forms.CharField(
+        max_length=LASTNAME_MAX_LEN,
         # required=False,
-        max_length=100,
         widget=forms.TextInput(
             attrs={
                 "id": "last_name_id",
@@ -76,7 +89,7 @@ class UserEditForm(forms.ModelForm):
     )
     phone = forms.CharField(
         required=False,
-        max_length=15,
+        max_length=PHONE_MAX_LEN,
         widget=forms.TextInput(
             attrs={
                 "id": "phone_id",
@@ -134,28 +147,30 @@ class UserEditForm(forms.ModelForm):
         if result is None:
             raise forms.ValidationError(_("数値以外は記入しないでください"))
 
-        if len(phone) > 15:
+        if len(phone) > PHONE_MAX_LEN:
             raise forms.ValidationError(_("正しい電話番号を入力してください"))
         return phone
 
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get("last_name")
+        if len(last_name) > LASTNAME_MAX_LEN:
+            # print(f"phne Error:{last_name=}")
+            raise forms.ValidationError(_("64文字以内にしてください"))
+        return last_name
+
     def clean_first_name(self):
         first_name = self.cleaned_data.get("first_name")
-        if len(first_name) > 64:
+        if len(first_name) > LASTNAME_MAX_LEN:
             # print(f"phne Error:{first_name=}")
             raise forms.ValidationError(_("64文字以内にしてください"))
         return first_name
-
-    def clean_last_name(self):
-        last_name = self.cleaned_data.get("last_name")
-        if len(last_name) > 100:
-            # print(f"phne Error:{last_name=}")
-            raise forms.ValidationError(_("100文字以内にしてください"))
-        return last_name
 
 
 class ChangePasswordForm(PasswordChangeForm):
     old_password = forms.CharField(
         required=True,
+        min_length=PASSWORD_MIN_LEN,
+        max_length=PASSWORD_MAX_LEN,
         widget=forms.PasswordInput(
             attrs={
                 "id": "old_password_id",
@@ -168,6 +183,8 @@ class ChangePasswordForm(PasswordChangeForm):
     )
     new_password1 = forms.CharField(
         required=True,
+        min_length=PASSWORD_MIN_LEN,
+        max_length=PASSWORD_MAX_LEN,
         widget=forms.PasswordInput(
             attrs={
                 "id": "new_password_id1",
@@ -180,6 +197,8 @@ class ChangePasswordForm(PasswordChangeForm):
     )
     new_password2 = forms.CharField(
         required=True,
+        min_length=PASSWORD_MIN_LEN,
+        max_length=PASSWORD_MAX_LEN,
         widget=forms.PasswordInput(
             attrs={
                 "id": "new_password_id2",
@@ -194,6 +213,36 @@ class ChangePasswordForm(PasswordChangeForm):
     class Meta:
         model = FtUser
         fields = ["old_password", "new_password1", "new_password2"]
+
+    def clean_old_password(self):
+        old_password = super().clean_old_password()
+        if len(old_password) > PASSWORD_MAX_LEN:
+            logging.error("Old Password  Error")
+            raise forms.ValidationError(_("20文字以内にしてください"))
+        if len(old_password) < PASSWORD_MIN_LEN:
+            logging.error("Old Password  Error")
+            raise forms.ValidationError(_("8文字以上にしてください"))
+        return old_password
+
+    def clean_new_password1(self):
+        new_password1 = self.cleaned_data.get("new_password1")
+        if len(new_password1) > PASSWORD_MAX_LEN:
+            logging.error("New Password1  Error")
+            raise forms.ValidationError(_("20文字以内にしてください"))
+        if len(new_password1) < PASSWORD_MIN_LEN:
+            logging.error("New Password1  Error")
+            raise forms.ValidationError(_("8文字以上にしてください"))
+        return new_password1
+
+    def clean_new_password2(self):
+        new_password2 = self.cleaned_data.get("new_password2")
+        if len(new_password2) > PASSWORD_MAX_LEN:
+            logging.error("New Password2  Error")
+            raise forms.ValidationError(_("20文字以内にしてください"))
+        if len(new_password2) < PASSWORD_MIN_LEN:
+            logging.error("New Password2  Error")
+            raise forms.ValidationError(_("8文字以上にしてください"))
+        return new_password2
 
     # フォーム全体へのバリデーション処理を追加する場合はここ。
     def clean(self):
