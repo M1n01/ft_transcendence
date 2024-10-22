4213c9d39a07f835d44a4685a11739d51ddabab3
hsano <hsano@student.42tokyo.jp>
2024-10-22 23:29:33 +0900

    fix ゲーム画面でゲーム終了時に戻るボタンが表示されないバグ修正

--- django/frontend/src/pong/js/pong/addScore.js
+++ django/frontend/src/pong/js/pong/addScore.js
@@ -15,6 +15,7 @@ export const addScore = async (ball, area) => {
 
   const formData = new FormData(add_score_form);
   const response = await fetchAsForm(add_score_form, formData);
+
   if (response.error) {
     error_connect_message.hidden = false;
 
@@ -25,7 +26,7 @@ export const addScore = async (ball, area) => {
   let score2 = json['player2_score'];
   const score_text1 = document.getElementById('player1_current_score');
   const score_text2 = document.getElementById('player2_current_score');
-  const back_button = document.getElementById('back-button');
+  const back_button = document.getElementById('back-games-button');
   score_text1.textContent = score1;
   score_text2.textContent = score2;
 
@@ -33,12 +34,14 @@ export const addScore = async (ball, area) => {
     const player1_win = document.getElementById('player1-win');
     player1_win.src = crown_img;
     player1_win.hidden = false;
-    back_button.hidden = false;
+    back_button.style.display = 'block';
+    back_button.removeAttribute('hidden');
   }
   if (Number(score2) >= 5) {
     const player2_win = document.getElementById('player2-win');
     player2_win.src = crown_img;
     player2_win.hidden = false;
-    back_button.hidden = false;
+    back_button.style.display = 'block';
+    back_button.removeAttribute('hidden');
   }
 };
