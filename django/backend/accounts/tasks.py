b71ed759e6d65d052cb80c1595ce676d17143b1e
hsano <hsano@student.42tokyo.jp>
2024-10-25 00:31:28 +0900

    fix FtTmpUserが存在しない状況で削除しようとしてエラーになる不具合修正

--- django/backend/accounts/tasks.py
+++ django/backend/accounts/tasks.py
@@ -1,25 +1,37 @@
 from celery import shared_task
 from .models import FtTmpUser
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 @shared_task
 def delete_tmp_user(user_id):
-    user = FtTmpUser.objects.get(id=user_id)
-    if user is not None:
-        user.delete()
+    try:
+        users = FtTmpUser.objects.filter(id=user_id)
+        if len(users) == 0:
+            return
+        users[0].delete()
+    except Exception as e:
+        logger.error(f"Delete Tmp User Error:{e}")
 
 
 @shared_task
 def change_login_state(user_id, flag):
-    user = FtTmpUser.objects.get(id=user_id)
-    if user is None:
+    users = FtTmpUser.objects.filter(id=user_id)
+    if len(users) == 0:
         return
+    user = users[0]
     user.is_login = flag
     user.save()
 
 
 @shared_task
 def check_login_state(user_id, flag):
-    user = FtTmpUser.objects.get(id=user_id)
+    users = FtTmpUser.objects.filter(id=user_id)
+    if len(users) == 0:
+        return
+    user = users[0]
     if user is None:
         return
