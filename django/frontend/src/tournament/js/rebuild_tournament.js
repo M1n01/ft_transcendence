dfce5f1147332e1582364bd03a4c18cb6675c1f6
hsano <hsano@student.42tokyo.jp>
2024-10-24 20:24:00 +0900

    fix トーナメント開始前にトーナメント表が出ている不具合修正

--- django/frontend/src/tournament/js/rebuild_tournament.js
+++ django/frontend/src/tournament/js/rebuild_tournament.js
@@ -7,24 +7,28 @@ import { fetchJsonData } from '../../spa/js/utility/fetch.js';
 export const RebuildTournamentEvent = new Event('RebuildTournamentEvent');
 
 document.addEventListener('RebuildTournamentEvent', async () => {
-  const chart_element = document.getElementById('tournament-chart');
-  if (!chart_element) {
+  try {
+    const chart_element = document.getElementById('tournament-chart');
+    if (!chart_element) {
+      return;
+    }
+    const id = document.getElementById('id-hidden');
+    const url = '/tournament/info/' + id.value;
+    const json = await fetchJsonData(url);
+    const tournament = document.getElementById('tournament-div'); // 既存の要素を取得
+    const totalParticipants = json['max_user_cnt'];
+
+    const chart = new TournamentChart(tournament, totalParticipants);
+    chart.init();
+
+    const matches = json['matches'];
+
+    if (chart.setGames(matches) == false) {
+      return false;
+    }
+    chart.draw();
+    chart.drawParticipants();
+  } catch {
     return;
   }
-  const id = document.getElementById('id-hidden');
-  const url = '/tournament/info/' + id.value;
-  const json = await fetchJsonData(url);
-  const tournament = document.getElementById('tournament-div'); // 既存の要素を取得
-  const totalParticipants = json['max_user_cnt'];
-
-  const chart = new TournamentChart(tournament, totalParticipants);
-  chart.init();
-
-  const matches = json['matches'];
-
-  if (chart.setGames(matches) == false) {
-    return false;
-  }
-  chart.draw();
-  chart.drawParticipants();
 });
